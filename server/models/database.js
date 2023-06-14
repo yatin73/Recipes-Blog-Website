f3c83097f818fbc0a67c03ccbc1863505eb60fcf
yatin73 <yatinrathod612@gmail.com>
2023-06-14 21:20:08 +0530

    Added Dockerfile

--- server/models/database.js
+++ server/models/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, authSource: 'admin'});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection Error: '));
