4271f9afb64dfd7356a0ab7b6c9808c2660028f8
yatin73 <60352795+yatin73@users.noreply.github.com>
2023-06-20 13:04:53 +0530

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, authSource: 'admin'});
+const MONGODB_URI = "mongodb+srv://yatin73:Yeahtinnn73@recipe-site-cluster.oodlj.mongodb.net/Recipes?retryWrites=true&w=majority";
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, authSource: 'admin'});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection Error: '));
